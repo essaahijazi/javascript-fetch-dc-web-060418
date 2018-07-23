06836b1afef1c83372afa1ed86b49b4ef8478469
Essa Hijazi <github email address>
2018-07-23 11:04:10 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 const app = "I don't do much.";
+const token = '014220aafb19bcdf5420c17328cfb5d08ba04245';
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
